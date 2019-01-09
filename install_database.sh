18d352b269b60c88983e93c6ab6da87b47ad9619
PedroHenriqueBraga <pedrohenriquebraga735@gmail.com>
2019-01-09 20:27:04 -0200

    Consegui organizar melhor a instalação da base de dados eseparar alimentação em arquivo diferente.

--- install_database.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-source .config
-
-mysqladmin -u$DB_USER -p$DB_PASSWORD -h 0.0.0.0  create $DB_NAME
-mysql -u$DB_USER -p$DB_PASSWORD -h 0.0.0.0  $DB_NAME < database/database.sql
